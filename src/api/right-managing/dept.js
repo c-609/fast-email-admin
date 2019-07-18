47e39c1f45fe4d8ddd19ee776748f162565502ed
moon927 <411840982@qq.com>
2019-07-18 16:21:58 +0800

    部门管理

--- src/api/right-managing/dept.js
+++ src/api/right-managing/dept.js
@@ -1,7 +1,7 @@
 import request from 'axios'
 
 //获取部门树
-export function getDeptTree(){
+export function getDeptTree() {
   return request({
     url: '/manage/dept/tree',
     method: 'get',
@@ -9,54 +9,64 @@ export function getDeptTree(){
 }
 
 //根据部门id获取部门信息
-export function getDeptInfo(id){
+//通过id查询信息
+export function getDeptInfo(id) {
   return request({
-    url: '/upms/dept/'+id,
-    method:'get',
+    // url: '/upms/dept/'+id,
+    url: '/manage/dept/' + id,
+    method: 'get',
   })
 }
 
-export function addDept(name,parentId,sort){
+//添加部门
+export function addDept(name, parentId, sort) {
   const data = {
     "name": name,
     "parentId": parentId
   }
   return request({
-    url: '/upms/dept/dept_add',
+    // url: '/upms/dept/dept_add',
+    url: '/manage/dept',
     method: 'post',
     params: data,
-    transformRequest: [function(){
+    transformRequest: [function () {
       return JSON.stringify(data)
     }],
     headers: {
-      'Content-Type' : 'application/json;'
+      'Content-Type': 'application/json;'
     }
   })
 }
 
-export function deleteDept(id){
+//删除部门
+export function deleteDept(id) {
   return request({
-    url: '/upms/dept/dept_delete',
-    method: 'post',
-    params: {id}
+    // url: '/upms/dept/dept_delete',
+    url: '/manage/dept/' + id,
+    method: 'delete',
+    params: {
+      id
+    }
   })
 }
 
-export function updateDept(id,name,parentId){
+//编辑部门
+export function updateDept(id, name, parentId) {
   const data = {
     "id": id,
     "name": name,
     "parentId": parentId
   }
   return request({
-    url: '/upms/dept/dept_update',
+    // url: '/upms/dept/dept_update',
+    url: '/manage/dept/update',
     method: 'post',
     params: data,
-    transformRequest: [function(){
+    transformRequest: [function () {
       return JSON.stringify(data)
     }],
     headers: {
-      'Content-Type' : 'application/json;'
+      'Content-Type': 'application/json;'
     }
   })
 }
--- src/components/pages/right-managing/dept/DeptTools.vue
+++ src/components/pages/right-managing/dept/DeptTools.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
-  <span class="dept-label">{{DATA.name}}</span>
-  <span  v-if="true" style="margin-left:15px" class="dept-btn">
-    <!-- <el-button type="text" class="el-icon-plus" @click.stop="nodeAdd(STORE,DATA,NODE)"></el-button> -->
-    <el-button type="text" class="el-icon-edit" @click.stop="nodeEdit(DATA)"></el-button>
-    <el-button type="text" class="el-icon-delete" @click.stop="nodeDel(DATA)"></el-button>
-  </span>
+    <span class="dept-label">{{DATA.name}}</span>
+    <span v-if="true" style="margin-left:15px" class="dept-btn">
+      <!-- <el-button type="text" class="el-icon-plus" @click.stop="nodeAdd(STORE,DATA,NODE)"></el-button> -->
+      <el-button type="text" class="el-icon-edit" @click.stop="nodeEdit(DATA)"></el-button>
+      <el-button type="text" class="el-icon-delete" @click.stop="nodeDel(DATA)"></el-button>
+    </span>
   </div>
 </template>
 
 <script>
-import eventBus from "./../../../../utils/eventBus.js"
-import { deleteDept} from '../../../../api/right-managing/dept';
+import eventBus from "./../../../../utils/eventBus.js";
+import { deleteDept } from "../../../../api/right-managing/dept";
 export default {
-  name:'DeptTools',
-  inject:['reload'],
-  props: ['DATA'],
-  data(){
-    return{
-      id: '',
-    }
+  name: "DeptTools",
+  inject: ["reload"],
+  props: ["DATA"],
+  data() {
+    return {
+      id: ""
+    };
   },
   // created:function(){
   //   eventBus.$on("getDeleteId",(id)=>{
@@ -30,42 +30,48 @@ export default {
     // nodeAdd(s,d,n){//新增
     //   this.$emit('nodeAdd',s,d,n)
     // },
-    nodeEdit(d){//编辑 
-      eventBus.$emit('deptNodeEdit');
+    nodeEdit(d) {
+      //编辑
+      eventBus.$emit("deptNodeEdit");
     },
-    nodeDel(d){//删除
+    nodeDel(d) {
+      //删除
       // this.$emit('nodeDel',s,d,n)
       // console.log(s)
       // console.log(d)该节点的数据
       // console.log(n)
-      if(d.children.length>0){
-         this.$message.error("此节点有子级，不可删除！")
-      }else{
+      if (d.children.length > 0) {
+        this.$message.error("此节点有子级，不可删除！");
+      } else {
         var _this = this;
-        this.id = d.id;   
-        this.$confirm('确定删除?', '提示', {
-          confirmButtonText: '确定',
-          type: 'warning'
-        }).then(() => {
-          deleteDept(_this.id).then((res)=>{
-            this.reload();
-          });
-        }).catch(() => {
-          _this.$message({
-            type: 'info',
-            message: '取消'
-          });
+        this.id = d.id;
+        this.$confirm("确定删除?", "提示", {
+          confirmButtonText: "确定",
+          type: "warning"
+        })
+          .then(() => {
+            deleteDept(_this.id).then(res => {
+              console.log(res);
+              if (res.data.code == 0) {
+                this.$message.success("删除成功！");
+                this.reload();
+              } else this.$message.error("删除失败！");
+            });
+          })
+          .catch(() => {
+            _this.$message({
+              type: "info",
+              message: "取消"
+            });
           });
       }
-      
-      
     }
   }
-}
+};
 </script>
 
 <style scoped>
- .dept-btn{
-  display:none
+.dept-btn {
+  display: none;
 }
 </style>
